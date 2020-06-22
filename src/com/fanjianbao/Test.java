24ab41b83b98078b405ad23e54b2585c2a04871d
fanjianbao123 <778608875@qq.com>
2020-06-22 19:31:15 +0800

    version 1

---
+++ src/com/fanjianbao/Test.java
@@ -0,0 +1,11 @@
+package com.fanjianbao;
+
+/**
+ * @author fanjianbao
+ * @date 2020/6/22 - 19:26
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world !!!");
+    }
+}
