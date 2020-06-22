9e4dcb2e27318cefce852d4d20704b471af43ae5
fanjianbao123 <778608875@qq.com>
2020-06-22 22:04:44 +0800

    version 2

---
+++ src/com/fanjianbao/Test2.java
@@ -0,0 +1,11 @@
+package com.fanjianbao;
+
+/**
+ * @author fanjianbao
+ * @date 2020/6/22 - 22:01
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
